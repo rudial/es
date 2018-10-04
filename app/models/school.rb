12e0beba4ddc1e9716d7721751db9ba0f7d8e490
Rudi <sash_kov@mail.ru>
2018-10-04 15:33:27 +0800

    pushing

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,8 +1,6 @@
 class School < ApplicationRecord
-	validates :Name, :Address, :Call_number, presence: true
+	validates :Name, :Address, presence: true
 	validates :Name, :Address, uniqueness: true
-	validates :Call_number, allow_blank: true, format: {
-	with: %r{(\d{2}-){2}\d{2}},
-	message: ':you must use only numbers and six symbols'
-	}
+	validates :Call_number, format: {
+	with: %r{\A(\d{2}-){2}\d{2}} }, allow_blank: true
 end
